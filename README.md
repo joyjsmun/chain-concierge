3b4264c4e902b6f3c6db5812f436aae11355d621
Cailyn Yong <>
2023-09-02 21:50:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Overview
+
+This is DAO Concierge, a bot that caters to all the needs you have for your DAO. From onboarding new users, asking questions about the DAO events, and automating the fundraising process for your DAO, DAO concierge will do it for you.
+
+This uses Langchain.
+
+---
+
+## To run
+
+Make sure to fill in the API keys for OpenAI and install all the dependencies. If not working, check the error log on streamlit site, they will let you know which ones you have to install.
+
+```
+python -m venv venv
+source venv/bin/activate
+pip install openai langchain
+pip install openai chromadb
+pip install bs4
+pip install python-dotenv
+pip install tiktoken
+pip install streamlit
+streamlit run chatbot.py
+
+```
---
+++ chatbot.py
@@ -0,0 +1,55 @@
+import dotenv
+dotenv.load_dotenv()
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import WebBaseLoader
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+import streamlit as st
+st.title("Chain Reporter")
+
+def get_text():
+    input_text = st.text_input("Type in the governance proposal below", key="input")
+    return input_text 
+
+user_input = get_text()
+
+if(user_input):
+    loader = WebBaseLoader(user_input)
+    data = loader.load()
+
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
+    all_splits = text_splitter.split_documents(data)
+
+
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+
+    question = "Summarize the main points of this proposal"
+    docs = vectorstore.similarity_search(question)
+    len(docs)
+
+    template = """Use the following pieces of context to answer the question at the end. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+    Use three sentences maximum. Return the result as a list. 
+    {context}
+    Question: {question}
+    Helpful Answer:"""
+    QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=vectorstore.as_retriever(),
+        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+    )
+
+if(user_input):
+    result = qa_chain({"query": question})
+    result["result"]
+    # st.write('**Result:** \n')
+    # st.write(result)
+
+st.button("Send to gmail")
